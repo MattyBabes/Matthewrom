2dcff6dc2861ea84256ccfa3de5c853fd008ee9a
MattyBabes <167733187+MattyBabes@users.noreply.github.com>
2024-05-04 22:37:14 -0700

    Add files via upload

---
+++ matthew Portfolio/main.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed(".text", {
+    strings: ["a BSIT-1C student.", "a dreamer with goals that is yet to be achieved."],
+    typeSpeed:40,
+    backSpeed:40,
+    backDelay:1000,
+    loop:true});
+
+
+
+    
